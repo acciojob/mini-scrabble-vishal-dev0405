cf42c5578e61869ead9dbc7e5a8aeef26a4484b4
Vishal Verma <100039120+vishal-dev0405@users.noreply.github.com>
2023-01-23 08:51:10 +0530

    created a submission

--- script.js
+++ script.js
@@ -1,1 +1,9 @@
 //your code here
+const textbox = document.getElementById("evaluatedText");
+const letterCount = document.getElementById("letterCount");
+
+textbox.addEventListener("input", updateLetterCount);
+
+function updateLetterCount() {
+  letterCount.innerHTML = textbox.value.length;
+}
